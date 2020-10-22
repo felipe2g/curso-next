65ac4c56e7a417236024d9516d8ebd3825857995
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-10-21 23:47:53 -0300

    adiciona componente de modal quando cliente adiciona produto ao carrinho

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,12 +1,23 @@
 //Dynamic Routing using [slug]
 import { route } from 'next/dist/next-server/server/router';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import AddToCartModal from '../../../components/AddToCartModal';
 
 export default function Products(){
-
   const router = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+
+  function handleAddToCard() {
+    setIsAddToCartModalVisible(true);
+  }
 
   return (
-    <h1>{router.query.slug}</h1>
+    <div>
+      <h1>{router.query.slug}</h1>
+      <button onClick={handleAddToCard}>Add to cart</button>
+
+      {isAddToCartModalVisible && <AddToCartModal /> }
+    </div>
   )
 }
